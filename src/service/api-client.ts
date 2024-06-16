9e94021cc9d8bd70eef0e51fd82f070bbe7cd8f2
cham1235 <hana171819@gmail.com>
2024-06-16 20:35:36 +0900

    fix api url

--- src/service/api-client.ts
+++ src/service/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api/games",
+  baseURL: "https://api.rawg.io/api",
   params: {
     key: import.meta.env.VITE_API_KEY,
   },
